ab6f0a93c5aef6c05c3e2a803ee595a9c1f8e1dd
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-03-19 18:16:30 -0300

    Revert "rectangulos"
    This reverts commit 028c06ed40c33b7e4f5935159830fcaa6ca2a3ad.

--- calculos/rectangulos.py
+++ calculos/rectangulos.py
@@ -1,15 +1,10 @@
-from sympy import sympify, lambdify, symbols, exp
-
-def rectangulos(f_x, a, b, n):
-    f_x = f_x.replace("sec", "1/cos")
-    f_x = f_x.replace("e", str(exp(1)))
-    x = symbols('x')
-    expr = sympify(f_x)
-    f = lambdify(x, expr, 'numpy')
-
+def f(x):
+    return eval(function_entry.get())
+def rectangulos(a, b, n):
     h = (b-a)/n
     integral = 0
     for i in range(n):
+        print(i)
         integral += f(a + i * h)
 
     return integral * h
