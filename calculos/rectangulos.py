ebc4d4ebb99efc9afe76cd84b1fb50742d51d366
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-03-19 16:59:22 -0300

    rectangulos

--- calculos/rectangulos.py
+++ calculos/rectangulos.py
@@ -1,6 +1,11 @@
-def rectangulos(a,b,n):
+def f(x):
+    return eval(function_entry.get())
+def rectangulos(a, b, n):
     h = (b-a)/n
-    for i in range(n-1):
-        integral += f(i)
+    integral = 0
+    for i in range(n):
+        print(i)
+        integral += f(a + i * h)
 
     return integral * h
+
