3c0a0106efd2156fafaf863a52b280407905623f
Juli-Gamietea <julian_gamietea@hotmail.com>
2023-03-19 18:29:55 -0300

    adding gitignore

--- calculos/rectangulos.py
+++ calculos/rectangulos.py
@@ -1,6 +1,12 @@
-def f(x):
-    return eval(function_entry.get())
-def rectangulos(a, b, n):
+from sympy import sympify, lambdify, symbols, exp
+def rectangulos(f_x, a, b, n):
+
+    f_x = f_x.replace("sec", "1/cos")
+    f_x = f_x.replace("e", str(exp(1)))
+    x = symbols('x')
+    expr = sympify(f_x)
+    f = lambdify(x, expr, 'numpy')
+
     h = (b-a)/n
     integral = 0
     for i in range(n):
