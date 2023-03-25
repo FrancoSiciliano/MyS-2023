458de9905d66509f47a38a4bf8afa91e8b735f9b
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2023-03-24 21:50:00 -0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+from metodos import AlgoritmoEulerMejorado, AlgoritmoEuler
+
 sys.path.append('.\\utils')
 sys.path.append('.\\metodos')
-from metodos import AlgoritmoEulerMejorado, AlgoritmoEuler
 
 res = AlgoritmoEulerMejorado("x + t", 0, 5, 10)
 print(res.calcular())
